cc4da2c54d7dca62f3825a41a8296dee21945bb1
JOEPAN <dou93739@gmail.com>
2025-12-13 16:13:33 +0800

    chore: prepare for release 1.6.0 (update docs, remove dev artifacts)

--- dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CURRENT_DATA_VERSION = void 0;
-// Data format version - increment when making breaking changes to the data structure
-// v2: Added lastModified to cards, pinnedFiles, stashCards for sidebar features
-exports.CURRENT_DATA_VERSION = 2;
-//# sourceMappingURL=types.js.map
